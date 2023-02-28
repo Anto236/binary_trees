c6ddbcfac173a14390138ab4c061a7e3d6f27bdf
Anto236 <Antonymbugua1237@gmail.com>
2023-02-28 19:37:35 +0000

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,75 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- *
- * @tree: pointer to the root node of the tree to measure the balance factor
- *
- * Return: balance factor of the tree, 0 if tree is NULL
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (left_height - right_height);
-}
-
-
-/**
- * binary_tree_depth - measures the depth of a node in a binary tree
- * @tree: a pointer to the node to measure the depth
- * Return: the depth of the tree, 0 if @tree == NULL
- */
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	size_t count;
-
-	if (tree == NULL || tree->parent == NULL)
-		return (0);
-
-	count = binary_tree_depth(tree->parent);
-	count++;
-	return (count);
-}
-
-/**
- * binary_tree_height - finds the height of a particular node
- * @tree: the node to find the height of
- * Return: ldepth if it is the largest node length or rdepth if not
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t ldepth;
-	size_t rdepth;
-
-	if (!tree)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	if (tree->left)
-		ldepth = binary_tree_height(tree->left) + 1;
-	else
-		ldepth = 0;
-
-	if (tree->right)
-		rdepth = binary_tree_height(tree->right) + 1;
-	else
-		rdepth = 0;
-
-	if (ldepth > rdepth)
-		return (ldepth);
-	else
-		return (rdepth);
-}
-
-/**
 * binary_tree_is_full - checks if a binary tree is full
 *
 * @tree: a pointer to the root node of the tree to check
@@ -88,24 +18,53 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	return (0);
 }
+
 /**
-* binary_tree_is_perfect - checks if a binary tree is perfect
+* binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+*
+* @tree: pointer to the root to check
+* Return: 1 if node is root, o if not or node is NULL
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	leaves += binary_tree_nodes(tree->left);
+	leaves += binary_tree_nodes(tree->right);
+
+	return (leaves + 1);
+}
+
+/**
+* binary_tree_is_perfect - function that checks if a binary tree is perfect
 *
 * @tree: the root
 * Return: If tree is NULL, your function must return 0
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int left_nodes, right_nodes;
+
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
-	{
-		int ldepth = binary_tree_depth(tree->left);
-		int rdepth = binary_tree_depth(tree->right);
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
 
-		if (ldepth == rdepth)
-			return (1);
-	}
-	return (0);
+	if (binary_tree_is_full(tree) == 0)
+		return (0);
+
+	left_nodes = binary_tree_nodes(tree->left);
+	right_nodes = binary_tree_nodes(tree->right);
+
+	if (left_nodes == right_nodes)
+		return (1);
+	else
+		return (0);
 }
