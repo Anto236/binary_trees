365d0c18959c35082ce8290acb247ca52ebcb1dd
Anto236 <Antonymbugua1237@gmail.com>
2023-02-28 14:02:19 +0000

    task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
-* binary_tree_is_leaf - function that checks if a given node is a root
+* binary_tree_is_root - function that checks if a given node is a root
 *
 * @node: pointer to the node to check
 * Return: 1 if node is root, o if not or node is NULL
