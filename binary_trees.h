6bdd477e78905d0daf2802a7d8acbc331eff4fdf
Anto236 <Antonymbugua1237@gmail.com>
2023-03-02 10:46:20 +0000

    task 34

--- binary_trees.h
+++ binary_trees.h
@@ -74,4 +74,6 @@ avl_t *inorder_sucessor(avl_t *tree, int mode);
 void change_node(avl_t **arg_tree, avl_t **arg_node);
 avl_t *avl_search_remove(avl_t **tree, int value);
 avl_t *avl_remove(avl_t *root, int value);
+void create_tree(avl_t **node, int *array, size_t size, int mode);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 #endif /* _BINARY_TREES_H_ */
