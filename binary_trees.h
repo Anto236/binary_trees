da8a8476971a3bdb8e9f3299c5229d9ca648653a
Anto236 <Antonymbugua1237@gmail.com>
2023-03-02 10:25:51 +0000

    task 28

--- binary_trees.h
+++ binary_trees.h
@@ -59,5 +59,8 @@ bst_t *bst_in(bst_t **tree, int value);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
+bst_t *r_case(bst_t *node, bst_t *root);
+bst_t *r_l_case(bst_t *node, bst_t *root);
+bst_t *bst_remove(bst_t *root, int value);
 
 #endif /* _BINARY_TREES_H_ */
